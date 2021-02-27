37e0f7e81b17d969001c2167929b06c6f309fbf6
Austin Serna <austinserna@austins-mbp.attlocal.net>
2021-02-26 23:19:30 -0500

    pushing student.py

---
+++ hw-02/Student.py
@@ -0,0 +1,45 @@
+# The Student class (you'll edit and expand on this)
+class Student():
+    '''
+    This class is designed to include information about individual students.
+    Currently this class has the following attributes:
+    
+    name : this is the student's name
+    gpa : this is the student's curret gpa
+    '''
+    
+    def __init__(self, name='', gpa=0.0, year = 0,):
+        self.name = name
+        self.gpa = gpa
+        self.courses = []
+    
+    def get_name(self):
+        '''
+        This function prints the name of the student
+        '''
+        print("My name is", self.name)
+    def enroll(self,courses):
+        '''
+        this function takes a list of course and adds them as an attribute to the student
+        '''
+        self.courses =
+        
+    def display_course(self):
+        '''
+        prints out courses enrolled
+        '''
+        
+        
+    def years_until_gradution(self):
+        '''
+        returns number of years left until a student graduates assuming normal four years
+        '''
+class Spartan():
+    
+    def __init__(self):
+        super().___init__(self)
+        
+    def set_motto(self,
+        
+        
+        
